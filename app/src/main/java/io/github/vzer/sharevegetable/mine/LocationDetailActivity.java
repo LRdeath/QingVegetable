88b4c0b47a882fbf92bf181535f69af3cbec6543
杨慈航 <yangcihang@hrsoft.net>
2017-08-14 17:28:24 +0800

    增加选择收货地址的功能
    Change-Id: I1fcb4e8e066b044d7e919447410c162368b416cb

--- app/src/main/java/io/github/vzer/sharevegetable/mine/LocationDetailActivity.java
+++ app/src/main/java/io/github/vzer/sharevegetable/mine/LocationDetailActivity.java
@@ -2,10 +2,16 @@ package io.github.vzer.sharevegetable.mine;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.lljjcoder.citylist.CityListSelectActivity;
+import com.lljjcoder.citylist.bean.CityInfoBean;
+import com.lljjcoder.citypickerview.widget.CityPicker;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -26,7 +32,6 @@ public class LocationDetailActivity extends ToolbarActivityPresenter<LocationCon
         implements LocationContract.View {
     public static int CHANGE_LOCATION = 1;
     public static int ADD_LOCATION = 2;
-    public static int RESULT_CODE = 0x123;
     private int type; //type，是修改地址还是添加地址
     @BindView(R.id.ly_location)
     RelativeLayout locationLy;
@@ -94,17 +99,78 @@ public class LocationDetailActivity extends ToolbarActivityPresenter<LocationCon
      */
     @OnClick(R.id.ly_location)
     void onSelectLocation() {
-        // TODO: 17/8/14 forResult 
-        startActivityForResult(new Intent(this, LocationSelectActivity.class), RESULT_CODE);
+//        Intent intent = new Intent(this, CityListSelectActivity.class);
+//        startActivityForResult(intent, CityListSelectActivity.CITY_SELECT_RESULT_FRAG);
+        CityPicker cityPicker = new CityPicker.Builder(this)
+                .textSize(20)
+                .title("地址选择")
+                .backgroundPop(0xa0000000)
+                .titleBackgroundColor("#45c24c")
+                .titleTextColor("#000000")
+                .backgroundPop(0xa0000000)
+                .confirTextColor("#000000")
+                .cancelTextColor("#000000")
+                .province("河北省")
+                .city("秦皇岛市")
+                .district("海港区")
+                .textColor(Color.parseColor("#000000"))
+                .provinceCyclic(true)
+                .cityCyclic(false)
+                .districtCyclic(false)
+                .visibleItemsCount(7)
+                .itemPadding(10)
+                .onlyShowProvinceAndCity(false)
+                .build();
+        cityPicker.show();
+
+        //监听方法，获取选择结果
+        cityPicker.setOnCityItemClickListener(new CityPicker.OnCityItemClickListener() {
+            @Override
+            public void onSelected(String... citySelected) {
+                //省份
+                String province = citySelected[0];
+                //城市
+                String city = citySelected[1];
+                //区县（如果设定了两级联动，那么该项返回空）
+                String district = citySelected[2];
+                //邮编
+                String code = citySelected[3];
+                String result = province + city + district;
+                locationTxt.setText(result);
+            }
+
+            @Override
+            public void onCancel() {
+                ToastUtil.showToast("已取消");
+            }
+        });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RESULT_CODE && resultCode == resultCode) {
-            ToastUtil.showToast("返回数据成功");
-        }
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == CityListSelectActivity.CITY_SELECT_RESULT_FRAG) {
+//            if (resultCode == RESULT_OK) {
+//                if (data == null) {
+//                    return;
+//                }
+//                Bundle bundle = data.getExtras();
+//
+//                CityInfoBean cityInfoBean = (CityInfoBean) bundle.getParcelable("cityinfo");
+//
+//                if (null == cityInfoBean)
+//                    return;
+//
+//                //城市名称
+//                String cityName = cityInfoBean.getName();
+//                //纬度
+//                String latitude = cityInfoBean.getLongitude();
+//                //经度
+//                String longitude = cityInfoBean.getLatitude();
+//
+//            }
+//        }
+//    }
 
     /**
      * 点击删除
